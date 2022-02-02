2396d591ec0ff94a5945f42ab306a918bffaf230
sulimannrafael@gmail.com <sulimannrafael@gmail.com>
2022-02-01 22:02:51 -0300

    Projeto criado

---
+++ src/main/java/br/com/sulimann/projectspringbootnosqlmongodb/ProjectSpringbootNosqlMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.sulimann.projectspringbootnosqlmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootNosqlMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootNosqlMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sulimann/projectspringbootnosqlmongodb/ProjectSpringbootNosqlMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sulimann.projectspringbootnosqlmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootNosqlMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
