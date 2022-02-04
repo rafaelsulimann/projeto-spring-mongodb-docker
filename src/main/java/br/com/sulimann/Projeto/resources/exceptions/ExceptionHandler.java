7e76887db2e17b2e2a2b406c08b58ad816544ecd
Rafael Sulimann <sulimannrafael@gmail.com>
2022-02-04 16:07:58 -0300

    Obtendo um usuário por id

---
+++ src/main/java/br/com/sulimann/Projeto/resources/exceptions/ExceptionHandler.java
@@ -0,0 +1,24 @@
+package br.com.sulimann.Projeto.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.sulimann.Projeto.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        String error = "Object not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/br/com/sulimann/Projeto/services/UserService.java
+++ src/main/java/br/com/sulimann/Projeto/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.sulimann.Projeto.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.sulimann.Projeto.model.UserModel;
 import br.com.sulimann.Projeto.repository.UserRepository;
+import br.com.sulimann.Projeto.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,11 @@ public class UserService {
     public List<UserModel> findAll(){
         return repository.findAll();
     }
+
+    public UserModel findById(String id){
+        Optional<UserModel> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    
 }
