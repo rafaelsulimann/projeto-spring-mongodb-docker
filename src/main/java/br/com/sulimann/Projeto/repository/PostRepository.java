512b04544b3c1588f030751761e6762eaafb3cbe
Rafael Sulimann <sulimannrafael@gmail.com>
2022-02-08 17:06:55 -0300

    Consulta com vários critérios

--- src/main/java/br/com/sulimann/Projeto/repository/PostRepository.java
+++ src/main/java/br/com/sulimann/Projeto/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package br.com.sulimann.Projeto.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,6 +14,9 @@ public interface PostRepository extends MongoRepository<PostModel, String> {
 
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<PostModel> searchTitle(String text);
-    
+
     List<PostModel> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: { $gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<PostModel> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/br/com/sulimann/Projeto/services/PostService.java
+++ src/main/java/br/com/sulimann/Projeto/services/PostService.java
@@ -1,5 +1,6 @@
 package br.com.sulimann.Projeto.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,10 @@ public class PostService {
 
     public List<PostModel> findByTitle(String text){
         return repository.searchTitle(text);
+    }
+
+    public List<PostModel> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
