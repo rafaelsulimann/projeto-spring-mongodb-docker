fc601ddbf55b4b5ba052cce3437d12cf586a9526
Rafael Sulimann <sulimannrafael@gmail.com>
2022-02-08 00:47:16 -0300

    Consulta simples com @Query

--- src/main/java/br/com/sulimann/Projeto/repository/PostRepository.java
+++ src/main/java/br/com/sulimann/Projeto/repository/PostRepository.java
@@ -3,12 +3,16 @@ package br.com.sulimann.Projeto.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.sulimann.Projeto.model.PostModel;
 
 @Repository
 public interface PostRepository extends MongoRepository<PostModel, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<PostModel> searchTitle(String text);
     
     List<PostModel> findByTitleContainingIgnoreCase(String text);
 }
