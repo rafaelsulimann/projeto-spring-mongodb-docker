0fdde5805e58eb20ac0e3c06bec540fd3ea2fabf
Rafael Sulimann <sulimannrafael@gmail.com>
2022-02-19 18:18:53 -0300

    Implementaçao de projeto Spring Boot com MongoDB, utilizando Dockerfile e Docker-Compose

--- app/src/test/java/br/com/sulimann/ApplicationTests/ApplicationTest.java
+++ app/src/test/java/br/com/sulimann/ApplicationTests/ApplicationTest.java
@@ -2,8 +2,12 @@ package br.com.sulimann.ApplicationTests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+import br.com.sulimann.Projeto.Application;
+
+@SpringBootTest(classes = Application.class)
+@ContextConfiguration
 class ApplicationTest {
 
 	@Test
